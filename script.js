70a835ecfad967f9ac379d7584b0575b7fcc3033
ayle57 <allistair.jak@gmail.com>
2025-11-02 16:17:54 +0100

    feat: add bun to the project

--- script.js
+++ script.js
@@ -30,3 +30,42 @@ window.addEventListener('scroll', () => {
         span.style.color = `hsl(${baseHue + hueOffset}, 70%, 50%)`;
     });
 });
+
+// Projects slider
+const slider = document.getElementById('project-slider');
+const items = Array.from(document.querySelectorAll('.project-item'));
+const projectsSectionContainer = document.querySelector('.projectsSection-container');
+let visible = 3;
+
+function updateVisible() {
+    if (window.innerWidth <= 520) visible = 1;
+    else if (window.innerWidth <= 900) visible = 2;
+    else visible = 3;
+}
+
+updateVisible();
+window.addEventListener('resize', updateVisible);
+
+let idx = 0;
+const total = items.length;
+
+function slide() {
+    idx++;
+    if (idx > total - visible) idx = 0;
+
+    const itemWidth = items[0].offsetWidth;
+    const gap = parseInt(getComputedStyle(slider).gap) || 0;
+
+    const moveX = idx * (itemWidth + gap);
+    slider.style.transform = `translateX(-${moveX}px)`;
+}
+
+let timer = setInterval(slide, 1500);
+slider.addEventListener('mouseenter', () => {
+    clearInterval(timer);
+    projectsSectionContainer.style.backgroundColor = '#00ffae';
+});
+slider.addEventListener('mouseleave', () => {
+    timer = setInterval(slide, 2500);
+    projectsSectionContainer.style.backgroundColor = '#ffffff';
+});
